7d849f7039b72fefbc7817390c0d8b030ae65632
Bujee-Ob <sushiblox574@gmail.com>
2021-01-25 07:31:40 -0600

    hi

---
+++ Commands/github.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "github",
+    aliases: ["github", "g"],
+    description: "Gives a github",
+
+    execute(message) {
+        message.reply('Here is my github. https://github.com/Bujee-Ob/CrossroadsRPBot');
+    }
+}
